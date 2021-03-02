bf32c0a352c684287b2c22abb759e6c00e69c051
DrInnovatio <turing1723@gmail.com>
2021-03-02 22:37:18 +1000

    import pages to App.js.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Navbar from './components/Navbar.jsx'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Home from './components/pages/Home.jsx'
-// import Services from './components/pages/Services.jsx'
-// import Products from './components/pages/Products.jsx'
-// import SignUp from './components/SignInUp.jsx'
+import Services from './components/pages/Services.jsx'
+import Products from './components/pages/Products.jsx'
+import SignUp from './components/SignInUp.jsx'
 
 function App () {
   return (
